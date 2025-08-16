b68e2a49040add7739db0a29ae267b0fb5dd8418
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-16 16:27:46 +0530

    updates

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./Pages/Auth/Login";
+import SignUp from "./Pages/Auth/SignUp";
+import LandingPage from "./Pages/LandingPage";
+import Dashboard from "./Pages/Home/Dashboard";
+import InterviewPrep from "./Pages/InterviewPrep/InterviewPrep.jsx";
 
 function App() {
   return (
-    <div className='text-5xl'>App</div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
