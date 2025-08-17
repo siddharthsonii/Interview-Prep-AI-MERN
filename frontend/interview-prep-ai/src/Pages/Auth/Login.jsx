ac640b7766d862a2edfb5e3894ac6e773bcc45cc
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-17 18:39:00 +0530

    updates

--- frontend/interview-prep-ai/src/Pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/Pages/Auth/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+function Login({ setCurrentPage }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
