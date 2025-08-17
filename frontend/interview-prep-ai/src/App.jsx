816b5f7a9e3e5f84fee3b025939752e0eacfb5c9
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-17 17:15:13 +0530

    updates

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import Login from "./Pages/Auth/Login";
-import SignUp from "./Pages/Auth/SignUp";
 import LandingPage from "./Pages/LandingPage";
 import Dashboard from "./Pages/Home/Dashboard";
 import InterviewPrep from "./Pages/InterviewPrep/InterviewPrep.jsx";
@@ -16,8 +14,6 @@ function App() {
           {/* Default Route */}
           <Route path="/" element={<LandingPage />} />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route
             path="/interview-prep/:sessionId"
