25699bc9ae3bc197fafd5e3370cd510ad1816fd8
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-18 23:13:01 +0530

    Frontend Login Signup Complete

--- frontend/interview-prep-ai/src/Pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/Pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input"
+import { validateEmail } from "../../utils/helper";
 
 function Login({ setCurrentPage }) {
   const [email, setEmail] = useState("");
@@ -12,7 +13,31 @@ function Login({ setCurrentPage }) {
   // Handle Login Form Submit
   const handleLogin = async (e) => {
     e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if(!password){
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError("");
+
+    // Login API Call
+    try {
+      
+    } catch (error) {
+      if(error.response && error.response.data.message){
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
   };
+
   return (
     <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
       <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
