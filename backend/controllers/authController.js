213be42f27f5bc78e203feb6d1c622816fbdedab
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-19 19:37:25 +0530

    Auth controller Registration code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,16 +10,63 @@ const generateToken = (userId) => {
 // @desc   Register a new user
 // @route  POST /api/auth/register
 // @access Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    console.log(User)
+    const { name, email, password, profileImageUrl } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    console.log(userExists);
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+    });
+
+    // Return user data with JWT
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    })
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc   Login user
 // @route  POST /api/auth/login
 // @access Public
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc   Get user profile
 // @route  GET /api/auth/profile
 // @access Private (Requires JWT)
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 module.exports = { registerUser, loginUser, getUserProfile };
