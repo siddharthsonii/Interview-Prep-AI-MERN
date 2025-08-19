dbdf801f85d5938dd20dfe6d501cc0aeb42f6984
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-19 10:42:55 +0530

    Auth Routes Defined

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -14,10 +17,19 @@ app.use(
     })
 );
 
+// DB Connection
+connectDB();
+
 // Middleware
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
