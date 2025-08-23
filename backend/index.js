a47c5ea5d8144102ba03429a9a3b959d67e02c93
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-24 01:41:23 +0530

    AI Questions and Explanation  routes controller completed

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 
 const app = express();
 
@@ -30,8 +32,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
