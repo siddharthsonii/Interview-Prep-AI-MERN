1f66993cc6443ad587502d09a9d2277c90518427
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-23 15:30:00 +0530

    Session routes controller completed

---
+++ backend/routes/sessionRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  createSession,
+  getSessionById,
+  getMySessions,
+  deleteSession,
+} = require("../controllers/sessionController");
+const { protect } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Session Routes
+router.post("/create", protect, createSession);
+router.get("/my-sessions", protect, getMySessions);
+router.get("/:id", protect, getSessionById);
+router.delete("/:id", protect, deleteSession);
+
+module.exports = router;
