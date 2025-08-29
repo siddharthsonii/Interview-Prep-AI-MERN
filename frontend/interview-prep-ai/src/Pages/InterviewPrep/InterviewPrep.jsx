6b52f3dd01f247cc7d78d993d4892a4e08010f8a
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-29 16:19:55 +0530

    Interview Prep Page UI Done

--- frontend/interview-prep-ai/src/Pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/Pages/InterviewPrep/InterviewPrep.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import RoleInfoHeader from "./components/RoleInfoHeader";
 
 function InterviewPrep() {
+  const { sessionId } = useParams();
+
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+
+  // Fetch session data by session id
+  const fetchSessionDetailsById = async () => {};
+
+  // Generate Concept Explanation
+  const generateConceptExplanation = async (question) => {};
+
+  // Pin Question
+  const toggleQuestionPinStatus = async (questionId) => {};
+
+  // Add more questions to a session
+  const uploadMoreQuestions = async () => {};
+
+  useEffect(() => {
+    if (sessionId) {
+      fetchSessionDetailsById();
+    }
+  }, [sessionId]);
+
   return (
-    <div>InterviewPrep</div>
-  )
+    <DashboardLayout>
+      <RoleInfoHeader
+        role={sessionData?.role || ""}
+        topicsToFocus={sessionData?.topicsToFocus || ""}
+        experience={sessionData?.experience || ""}
+        questions={sessionData?.questions || "_ _"}
+        description={sessionData?.description || ""}
+        lastUpdated={
+          sessionData?.updatedAt
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
+        }
+      />
+    </DashboardLayout>
+  );
 }
 
-export default InterviewPrep
+export default InterviewPrep;
