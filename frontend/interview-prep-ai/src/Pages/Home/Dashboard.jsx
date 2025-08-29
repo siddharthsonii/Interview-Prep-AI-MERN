232ac120547f80dcc839dbd3700a5d3e7b72e5ed
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-29 11:34:53 +0530

    Dashboard Summary Card UI Done

--- frontend/interview-prep-ai/src/Pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/Pages/Home/Dashboard.jsx
@@ -4,6 +4,12 @@ import { CARD_BG } from "../../utils/data";
 import toast from "react-hot-toast";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosInstance";
+import SummaryCard from "../../components/Cards/SummaryCard";
+import moment from "moment";
+import Modal from "../../components/Modal";
+import CreateSessionForm from "./CreateSessionForm";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -16,7 +22,14 @@ function Dashboard() {
     data: null,
   });
 
-  const fetchAllSessions = async () => {};
+  const fetchAllSessions = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.SESSION.GET_ALL);
+      setSessions(response.data);
+    } catch (error) {
+      console.error("Error fetching session data:", error);
+    }
+  };
 
   const deleteSession = async (sessionData) => {};
 
@@ -27,14 +40,42 @@ function Dashboard() {
   return (
     <DashboardLayout>
       <div className="container mx-auto pt-4 pb-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0"></div>
-        <button className="h-12 md:h-12 flex items-center justify-center gap-3 bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 right-10 md:right-20" onClick={() => setOpenCreateModal(true)}>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0">
+          {sessions?.map((data, index) => (
+            <SummaryCard
+              key={data?._id}
+              colors={CARD_BG[index % CARD_BG.length]}
+              role={data?.role || ""}
+              topicsToFocus={data?.topicsToFocus || ""}
+              experience={data?.experience || "-"}
+              questions={data?.questions?.length || "-"}
+              description={data?.description || ""}
+              lastUpdated={
+                data?.updatedAt
+                  ? moment(data.updatedAt).format("Do MMM YYYY")
+                  : ""
+              }
+              onSelect={() => navigate(`/interview-prep/${data?._id}`)}
+              onDelete={() => setOpenDeleteAlert({ open: true, data })}
+            />
+          ))}
+        </div>
+        <button
+          className="h-12 md:h-12 flex items-center justify-center gap-3 bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 right-10 md:right-20"
+          onClick={() => setOpenCreateModal(true)}
+        >
           <LuPlus className="text-2xl text-white" />
           Add New
         </button>
       </div>
+
+      <Modal isOpen={openCreateModal} onClose={() => {setOpenCreateModal(false);}} hideHeader>
+        <div>
+          <CreateSessionForm />
+        </div>
+      </Modal>
     </DashboardLayout>
   );
-};
+}
 
 export default Dashboard;
--- frontend/interview-prep-ai/src/utils/helper.js
+++ frontend/interview-prep-ai/src/utils/helper.js
@@ -1,4 +1,17 @@
 export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
-}
+};
+
+export const genInitials = (title) => {
+    if(!title) return "";
+
+    const words = title.split(" ");
+    let initials = "";
+
+    for(let i=0; i<Math.min(words.length, 2); i++){
+        initials += words[i][0];
+    }
+
+    return initials.toUpperCase();
+};
