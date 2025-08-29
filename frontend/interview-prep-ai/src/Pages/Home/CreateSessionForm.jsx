1b8f13ec82feb22f67ab45604019df5e12c08d8c
Siddharth Soni <siddharth.soni588@gmail.com>
2025-08-29 13:12:46 +0530

    Session Form Popup Added

--- frontend/interview-prep-ai/src/Pages/Home/CreateSessionForm.jsx
+++ frontend/interview-prep-ai/src/Pages/Home/CreateSessionForm.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import SpinnerLoader from '../../components/Loader/SpinnerLoader';
 
 const CreateSessionForm = () => {
+    const [formData, setFormData] = useState({
+        role: "",
+        experience: "",
+        topicsToFocus: "",
+        description: "",
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleChange = (key, value) => {
+        setFormData((prevData) => ({
+            ...prevData, [key]: value,
+        }));
+    };
+
+    const handleCreateSession = async (e) => {
+        e.preventDefault();
+
+        const {role, experience, topicsToFocus} = formData;
+
+        if(!role || !experience || !topicsToFocus) {
+            setError("Please fill all the required fields.");
+            return;
+        }
+
+        setError("");
+    };
   return (
-    <div>CreateSessionForm</div>
-  )
+    <div className="w-[90vw] md:w-[35vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Start a New Interview Journey</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-3">
+        Fill out a few quick details and unlock your personalized set of
+        interview questions!
+      </p>
+
+      <form onSubmit={handleCreateSession} className="flex flex-col gap-3">
+        <Input
+          value={formData.role}
+          onChange={({ target }) => handleChange("role", target.value)}
+          label="Target Role"
+          placeholder="(e.g., Frontend Developer, UI/UX Designer, etc.)"
+          type="text"
+        />
+
+        <Input
+          value={formData.experience}
+          onChange={({ target }) => handleChange("experience", target.value)}
+          label="Years of Experience"
+          placeholder="(e.g., 1 year, 3 year, 5+ years)"
+          type="number"
+        />
+
+        <Input
+          value={formData.topicsToFocus}
+          onChange={({ target }) => handleChange("topicsToFocus", target.value)}
+          label="Topics to Focus On"
+          placeholder="(Comma-separated, e.g., React, Node.js, MongoDB)"
+          type="text"
+        />
+
+        <Input
+          value={formData.description}
+          onChange={({ target }) => handleChange("description", target.value)}
+          label="Description"
+          placeholder="(Any specific goals or notes for this session)"
+          type="text"
+        />
+
+        {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+        <button type='submit' className='btn-primary w-full mt-2' disabled={isLoading}>
+            {isLoading && <SpinnerLoader />} Create Session
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateSessionForm
